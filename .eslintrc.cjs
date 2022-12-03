84048eb8076c13b5457215eafe955d8385d01cad
typewriter <takuyaxp@gmail.com>
2022-12-03 13:29:51 +0900

    Add ESLint and Prettier

---
+++ .eslintrc.cjs
@@ -0,0 +1,27 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:react/recommended",
+    // Using new JSX transform (from React 17)
+    // See: https://github.com/jsx-eslint/eslint-plugin-react#configuration-legacy-eslintrc
+    "plugin:react/jsx-runtime",
+    "plugin:@typescript-eslint/recommended",
+  ],
+  settings: {
+    react: {
+      version: "detect",
+    },
+  },
+  overrides: [],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  plugins: ["react", "@typescript-eslint"],
+  rules: {},
+};
